ee9554bd3942043b1642c13d37d4333cc53ee21c
carmenfan <wind5320@hotmail.com>
2016-04-04 17:00:05 +0100

    #v1.0Beta update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
     QCoreApplication::setOrganizationName("3D Repo");
     QCoreApplication::setOrganizationDomain("3drepo.org");
     QCoreApplication::setApplicationName("3D Repo GUI");
-    QCoreApplication::setApplicationVersion("1.0.0b");
+    QCoreApplication::setApplicationVersion("1.0.0b2");
 
 	std::vector<repo::lib::RepoAbstractListener*> listeners;
 	listeners.push_back(repo::logger::RepoLogger::getInstance());
