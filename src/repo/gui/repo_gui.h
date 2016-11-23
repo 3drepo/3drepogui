80e444a61218f7b19757c80f62b72f93f3f18637
carmenfan <wind5320@hotmail.com>
2016-11-23 15:53:03 +0000

    Version 1.7.6

--- src/repo/gui/repo_gui.h
+++ src/repo/gui/repo_gui.h
@@ -31,7 +31,7 @@
 //------------------------------------------------------------------------------
 
 
-#define EXPECTED_BOUNCER_VERSION "1.7.3"
+#define EXPECTED_BOUNCER_VERSION "1.7.4"
 
 namespace Ui
 {
